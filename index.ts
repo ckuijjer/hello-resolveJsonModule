3fd7bae050d74270a461e7882ad9e5de13bb5fd7
Casper Kuijjer <casper@kuijjer.com>
2022-09-14 16:19:25 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import * as input from "./input.json";
+import defaultInput from "./input.json";
+import * as inputObject from "./input-object.json";
+import defaultInputObject from "./input-object.json";
+
+console.log("🧑‍💻 running index.ts");
+console.log("🧑‍💻 array *");
+console.log(input);
+
+console.log("🧑‍💻 array");
+console.log(defaultInput);
+
+console.log("🧑‍💻 object *");
+console.log(inputObject);
+
+console.log("🧑‍💻 object");
+console.log(defaultInputObject);
